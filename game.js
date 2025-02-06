b4d8527f8eee3097f192d7026c628920e66834e5
sweetpea98 <nn.leesen@gmail.com>
2025-02-06 17:33:38 +1100

    Update game.js

--- game.js
+++ game.js
@@ -3,3 +3,10 @@ const prizes = ["🧁 Cake!", "🍾 Wine!", "💄 Lipstick!"];
 function revealPrize(index) {
     document.getElementById("prize-message").innerHTML = `You got: ${prizes[index]}`;
 }
+
+document.addEventListener("click", function() {
+  let audio = document.getElementById("bg-music");
+  if (audio.paused) {
+    audio.play();
+  }
+});
